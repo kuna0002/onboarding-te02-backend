a87feb7ec8958906eb61937daee10795cdd13e38
Kunal <kuna0002@student.monash.edu>
2025-03-15 22:00:46 +1100

    initial commit

---
+++ src/main/java/com/monash/sunbuddy/SunBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.monash.sunbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monash/sunbuddy/SunBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monash.sunbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
